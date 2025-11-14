336ca30bd8bab2d449a195f6f33e1bff6dd19ce9
Noah48694062 <viethoang.hr@gmail.com>
2025-11-15 00:07:12 +0700

    bot ngu

--- QLCuaHangThuCung_BTL/QLCuaHangThuCung_BTL/ProductModule.cs
+++ QLCuaHangThuCung_BTL/QLCuaHangThuCung_BTL/ProductModule.cs
@@ -14,12 +14,12 @@ namespace QLCuaHangThuCung_BTL
         public ProductModule()
         {
             InitializeComponent();
-            this.Text = String.Format("About {0}", AssemblyTitle);
-            this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-            this.labelCopyright.Text = AssemblyCopyright;
-            this.labelCompanyName.Text = AssemblyCompany;
-            this.textBoxDescription.Text = AssemblyDescription;
+            //this.Text = String.Format("About {0}", AssemblyTitle);
+            //this.labelProductName.Text = AssemblyProduct;
+            //this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+            //this.labelCopyright.Text = AssemblyCopyright;
+            //this.labelCompanyName.Text = AssemblyCompany;
+            //this.textBoxDescription.Text = AssemblyDescription;
         }
 
         #region Assembly Attribute Accessors
