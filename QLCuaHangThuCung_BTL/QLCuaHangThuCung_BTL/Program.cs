123ffb2dc260d6be5d07988eccdd47014c1faa42
Noah48694062 <viethoang.hr@gmail.com>
2025-11-17 16:48:00 +0700

    update login

--- QLCuaHangThuCung_BTL/QLCuaHangThuCung_BTL/Program.cs
+++ QLCuaHangThuCung_BTL/QLCuaHangThuCung_BTL/Program.cs
@@ -16,7 +16,7 @@ namespace QLCuaHangThuCung_BTL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Login());
         }
     }
 }
